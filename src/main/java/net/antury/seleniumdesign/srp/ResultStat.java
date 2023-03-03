74664a175ae24cff556f87cc67de6ce7a726bc0f
Wilson Antury <ventas@antury.net>
2023-03-02 21:20:41 -0500

    add stat componentObject

---
+++ src/main/java/net/antury/seleniumdesign/srp/ResultStat.java
@@ -0,0 +1,24 @@
+package net.antury.seleniumdesign.srp;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class ResultStat extends AbstractComponent {
+
+    @FindBy(id = "resultStats")
+    private WebElement stats;
+
+    public ResultStat(WebDriver driver) {
+        super(driver);
+    }
+
+    public String getStat(){
+        return this.stats.getText();
+    }
+
+    @Override
+    public boolean isDisplayed() {
+        return this.wait.until(d-> stats.isDisplayed());
+    }
+}
